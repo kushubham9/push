15fa234a96f5f3835a4990b1ef1370d6af98c204
shubham <kumar.shubham@wingify.com>
2017-08-06 12:54:56 +0530

    import

--- background-worker.js
+++ background-worker.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/97f52e3fd7f3004f7725585a1953a9d1.js');
+importScripts('https://cdn.pushcrewlocal.com/sw/97f52e3fd7f3004f7725585a1953a9d1.js');
