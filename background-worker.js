61c62d53a097ad952556c84d2555eacac853d7e2
shubham <kumar.shubham@wingify.com>
2017-08-11 15:51:33 +0530

    cheange

--- background-worker.js
+++ background-worker.js
@@ -1,23 +1,2 @@
-self.addEventListener('push', function(event) {
-  console.log('[Service Worker] Push Received.');
-  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
-
-  const title = 'Push Codelab';
-  const options = {
-    body: 'Yay it works.',
-    icon: 'images/icon.png',
-    badge: 'images/badge.png'
-  };
-
-  event.waitUntil(self.registration.showNotification(title, options));
-});
-
-self.addEventListener('notificationclick', function(event) {
-  console.log('[Service Worker] Notification click Received.');
-
-  event.notification.close();
-
-  event.waitUntil(
-    clients.openWindow('https://developers.google.com/web/')
-  );
-});
+var version = 6;
+importScripts('https://cdn.pushcrewlocal.com/sw/97f52e3fd7f3004f7725585a1953a9d1.js');
